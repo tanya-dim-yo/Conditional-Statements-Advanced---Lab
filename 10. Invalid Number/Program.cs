04e7c42f1fb43bbc606416fc4aa743725bfdb56a
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-02 04:29:20 +0300

    Add files via upload

---
+++ 10. Invalid Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InvalidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number < 100 && number != 0)
+            {
+                Console.WriteLine("invalid");
+            }
+            else if (number > 200)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
